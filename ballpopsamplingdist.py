3dc4a563434a0a376e4339c71f8acfe2a175627c
Minh Truong <minhtruong@Minhs-MacBook-Air.local>
2024-03-29 16:15:23 -0700

    init commit

---
+++ ballpopsamplingdist.py
@@ -0,0 +1,41 @@
+from random import choice, randint
+import numpy as np
+
+class PopulationSamplingDistribution():
+    def __init__(self, N: int) -> None:
+        self.N          = N
+        self.n          = 0
+        self.population = []
+        for i in range(N):
+            if randint(1, 10) <= 3:
+                self.population.append(1)
+            else:
+                self.population.append(0)
+
+        self.props      = []
+        self.succ       = []
+
+    def reset(self) -> None:
+        self.n, self.props, self.succ = 0, [], []
+
+    def generate_distribution(self, n: int) -> None:
+        sample          = [self.population.pop(randint(0, len(self.population)-1)) for i in range(n)] # n samples from self.population hypogeometrically
+        successes       = len(list(filter(lambda x: x == 1, sample))) # Record successes
+
+        self.props.append(successes/n)
+        self.succ.append(successes)
+
+        self.population += sample  # Redistribute into population
+        self.n          = n
+
+    def get_population_point_estimate(self) -> float:
+        mean = np.mean(self.succ)
+        std  = np.std(self.succ)
+
+        population = (std**2 + (mean * (mean - self.n))) / (std**2 + (mean / self.n) * (mean - self.n))
+
+        return population
+
+    def get_population_conf_int(self, conf_level: float) -> tuple:
+        pass
+
---
+++ min_sample_size.py
@@ -0,0 +1,33 @@
+from popsamplingdist import PopulationSamplingDistribution
+import numpy as np
+from os import system
+import matplotlib.pyplot as plt
+
+pop = 500
+system('clear')
+
+min_sample_num = []
+ratios         = []
+
+for n in range(1, pop+1):
+    ratios.append(n/pop)
+    dist = PopulationSamplingDistribution(N = pop)
+
+    for num_samples in range(1, pop+1):
+        for i in range(n):
+            dist.generate_distribution(n)
+        error = (dist.get_population_point_estimate() - pop)/pop
+        if abs(error) <= .1 or num_samples == pop:
+            min_sample_num.append(num_samples)
+            break
+
+plt.plot(ratios, min_sample_num)
+plt.xlabel("Ratio of sample size to population")
+plt.ylabel("Min Sample Size for 10% Error")
+plt.show()
+
+print(f"mean: {np.mean(dist.props)}")
+print(f"std: {np.std(dist.props)}")
+
+print(f"point estimate: {dist.get_population_point_estimate()}")
+
